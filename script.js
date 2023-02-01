b5647cfdbb2b5dcaf8632fa71f2b05d2841914f9
Stephany K <stephanykleen@gmail.com>
2023-02-01 15:36:52 -0800

    added functions to make icons responsive

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
 function initMap() {
-  new google.maps.Map(document.getElementById("map"), {
-    mapId: "ecc6ef3ca4958083",
-    center: { lat: 48.85, lng: 2.35 },
-    zoom: 12,
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.76432910263944, lng: -122.44670030095159},
+    zoom: 14,
+    mapId: 'ecc6ef3ca4958083',
   });
+
+
+  const markers = [[
+    "Mario's Choice: FRESCA",
+    37.76484303435829,
+    -122.46434270593578,
+    "images/mario.svg",
+    50,
+    50
+  ],
+  [
+    "Luigi's Choice: LIMON",
+    37.7648515934807,
+    -122.42224633155307,
+    "images/luigi.svg",
+    50,
+    50
+  ],
+  [
+    "Toad's Choice: SANGUCHON",
+    37.75547031997654,
+    -122.42053882282005,
+    "images/mushroom.svg",
+    50,
+    50
+  ],
+  [
+    "Super Mario's Choice: Lomo Libre Cantina",
+    37.76541404628794,
+    -122.47796237815434,
+    "images/fort.svg",
+    50,
+    50
+  ]
+];
+  for(let i = 0; i< markers.length; i++){
+    const currentMarker = markers[i];
+    const marker = new google.maps.Marker({
+      position: {lat: currentMarker[1], lng: currentMarker[2] },
+      map,
+      title: currentMarker[0],
+      icon: {
+        url: currentMarker[3],
+        scaledSize: new google.maps.Size(currentMarker[4],currentMarker[5]),
+      },
+      animation: google.maps.Animation.DROP
+    });
+  
+    const infoWindow = new google.maps.InfoWindow({
+      content: currentMarker[0],
+    });
+    marker.addListener("click", () => {
+      infoWindow.open(map, marker);
+      });
+  }
+  
 }
 
-window.initMap = initMap;
+//37.7648515934807,
